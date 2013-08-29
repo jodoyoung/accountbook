0106c809c54dc104fbc16d53aaa8ab3094ebf9c1
jodoyoung <jodoyoung36@gmail.com>
2013-08-30 00:10:02 +0900

    서브액티비티추가

--- src/org/anajo/accountbook/ExpenseActivity.java
+++ src/org/anajo/accountbook/ExpenseActivity.java
@@ -1,29 +1,26 @@
 package org.anajo.accountbook;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class ExpenseActivity extends Activity {
 
-	Button btnCreateExpense;
+	Button btn1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_expense);
 
-		btnCreateExpense = (Button) findViewById(R.id.btnCreateExpense);
-		btnCreateExpense.setOnClickListener(new OnClickListener() {
+		btn1 = (Button) findViewById(R.id.button1);
+		btn1.setOnClickListener(new OnClickListener() {
 			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(MainActivity.this,
-						ExpenseActivity.class);
-				startActivity(intent);
+			public void onClick(View arg0) {
+				finish();
 			}
 		});
 	}
